22b6de518d570216392b56d181fc2120632d270f
dekujon <joniantumabang@gmail.com>
2022-08-23 12:41:50 +0800

    Updated

--- eentrepreneur/javascript.js
+++ eentrepreneur/javascript.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
         $(".first").show();
         $("#des2, #des3, #des4, .title1").hide();
         $("#des1, .title2, .title3, .title4").show();
-        $(this).removeClass("goals").addClass("goalsx");
-        $("#goal2, #goal3, #goal4").removeClass("goalsx").addClass("goals");
+        $(this).removeClass("goals").addClass("goalsx").addClass("uno");
+        $("#goal2, #goal3, #goal4").removeClass("goalsx").removeClass("dos").removeClass("tres").removeClass("quatro").addClass("goals");
         })  
     
     $("#goal2").on("click", function(){
@@ -14,8 +14,8 @@ $(document).ready(function() {
         $(".second").show();
         $("#des1, #des3, #des4, .title2").hide();
         $("#des2, .title1, .title3, .title4").show();
-        $(this).removeClass("goals").addClass("goalsx");
-        $("#goal1, #goal3, #goal4").removeClass("goalsx").addClass("goals");
+        $(this).removeClass("goals").addClass("goalsx").addClass("dos");
+        $("#goal1, #goal3, #goal4").removeClass("goalsx").removeClass("uno").removeClass("tres").removeClass("quatro").addClass("goals");
         })
 
     $("#goal3").on("click", function(){
@@ -23,8 +23,8 @@ $(document).ready(function() {
         $(".third").show();
         $("#des1, #des2, #des4, .title3").hide();
         $("#des3, .title1, .title2, .title4").show();
-        $(this).removeClass("goals").addClass("goalsx");
-        $("#goal1, #goal2, #goal4").removeClass("goalsx").addClass("goals");
+        $(this).removeClass("goals").addClass("goalsx").addClass("tres");
+        $("#goal1, #goal2, #goal4").removeClass("goalsx").removeClass("uno").removeClass("dos").removeClass("quatro").addClass("goals");
         })    
 
     $("#goal4").on("click", function(){
@@ -32,15 +32,26 @@ $(document).ready(function() {
         $(".fourth").show();
         $("#des1, #des2, #des3, .title4").hide();
         $("#des4, .title1, .title2, .title3").show();
-        $(this).removeClass("goals").addClass("goalsx");
-        $("#goal1, #goal2, #goal3").removeClass("goalsx").addClass("goals");
+        $(this).removeClass("goals").addClass("goalsx").addClass("quatro");
+        $("#goal1, #goal2, #goal3").removeClass("goalsx").removeClass("uno").removeClass("dos").removeClass("tres").addClass("goals");
         })    
 
-
+    
     $(window).resize(function() {
-        if ($(this).width() < 992) {
-            $("#goal2, #goal3, #goal4").insertAfter(".after");
-
+        var width = $(window).width();
+        if (width < 991.98) {
+            if ($("#goal1").hasClass("uno")) {
+                $("#goal2, #goal3, #goal4").insertAfter(".after");
+              }
+            else if ($("#goal2").hasClass("dos")) {
+                $("#goal3, #goal4").insertAfter(".after");
+              }
+            else if ($("#goal3").hasClass("tres")) {
+                $("#goal4").insertAfter(".after");
+              }
+            else if ($("#goal4").hasClass("quatro")) {
+                $(this).insertAfter("#goal3");
+              }
             $("#goal1").on("click", function(){
                 $("#goal2, #goal3, #goal4").insertAfter(".after");
                 })  
@@ -66,7 +77,8 @@ $(document).ready(function() {
         });
 
     $(window).resize(function() {
-        if ($(this).width() > 992) {
+        var width = $(window).width();
+        if (width > 992) {
             $("#goal2, #goal3, #goal4").insertAfter("#goal1");
             
             $("#goal1").on("click", function(){
@@ -93,7 +105,7 @@ $(document).ready(function() {
 
     });
 
-    if (window.matchMedia('(max-width: 992px)').matches) {
+    if (window.matchMedia('(max-width: 991.98px)').matches) {
         $("#goal2, #goal3, #goal4").insertAfter(".after");
 
         $("#goal1").on("click", function(){
@@ -118,8 +130,9 @@ $(document).ready(function() {
         }) 
     
     }
+    
 
-    if (window.matchMedia('(min-width: 993px)').matches) {
+    if (window.matchMedia('(min-width: 992px)').matches) {
          $("#goal2, #goal3, #goal4").insertAfter("#goal1");
             
         $("#goal1").on("click", function(){
